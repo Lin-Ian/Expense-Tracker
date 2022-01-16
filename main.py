4c1161fa3c27034a2eac037cf13b25e5e4ae9882
Lin-Ian <ianrp.lin@gmail.com>
2022-01-16 12:27:18 -0500

    edit connection to database and create disconnection from database

--- main.py
+++ main.py
@@ -4,17 +4,38 @@ import sqlite3
 def connect_to_db():
     """
     Set up connection to sqlite database
-    :return: None
+    :return: connection object that represents the database
     """
 
+    # Connect to sqlite database
     connection = sqlite3.connect("identifier.sqlite")
     cursor = connection.cursor()
     print("Connected to sqlite")
+
+    # Create table if it does not exist
     cursor.execute('CREATE TABLE IF NOT EXISTS expenses(date TEXT, vendor TEXT, category TEXT, amount INT, notes TEXT)')
+    cursor.close()
+
+    # Return the connection to be used later on
+    return connection
+
+
+def disconnect_from_db(connection):
+    """
+    Close the connection to sqlite database
+    :param connection: connection object that represents the database
+    :return: None
+    """
+    connection.close()
+    print("Connection to sqlite closed")
 
 
 def main():
-    connect_to_db()
+    # Connect to database
+    connection = connect_to_db()
+
+    # Disconnect from database
+    disconnect_from_db(connection)
 
 
 if __name__ == "__main__":
